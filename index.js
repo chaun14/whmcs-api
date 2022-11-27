0c8735f427a3c675c2f2fbdb6eed0d2986fc1005
chaun14 <42867004+chaun14@users.noreply.github.com>
2022-11-27 15:01:32 +0100

    Move to undici for better error handling

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
 /**
  * @author Om Bhende
  * @module whmcs-api
-*/
-
-const fetch = require("node-fetch")
-const prepare = require("querystring")
-
+ */
+const { fetch } = require("undici");
+const prepare = require("querystring");
 
 class WHMCS {
-    /**
+  /**
      * Initializer.
      * @param {object} object 
      * @param {string} object.accesskey WHMCS API Accesskey
@@ -20,45 +18,53 @@ class WHMCS {
     * @param {string} object.identifier WHMCS API Username
     * @param {string} object.secret WHMCS API Password
     */
-    constructor(object) {
-        this.eval = {}  
-        for (let key in object) {
-            this.eval[key] = object[key]
-        }
+  constructor(object) {
+    this.eval = {};
+    for (let key in object) {
+      this.eval[key] = object[key];
+    }
     return this;
-    }
+  }
 
-    
-    /**
-     * Calling API
-     * @param {string} Action
-     * @param {object} API_Parameters
-     */
+  /**
+   * Calling API
+   * @param {string} Action
+   * @param {object} API_Parameters
+   */
 
-    call = (api, params) => {
-        this.eval.action = api
-        return new Promise((resolve, reject) => {
-            if (typeof(params) === "object") {
-                // Check the array.
-                for (let key in params) {
-                    if (params[key] === false) {
-                        reject(`Missing Object Parameters. Key: ${key}`)
-                    } else {
-                        this.eval[key] = params[key]
-                    }
-                }
-            }
-            fetch(`${this.eval.endpoint}?${prepare.stringify(this.eval)}`).then(res => {
-                res.json().then(data => {
-                    if (data.result === "success") {
-                        resolve(data)
-                    } else {
-                        reject(new Error(data.message))
-                    }
-                })
-            })
-        })
-    }
+  call = (api, params) => {
+    this.eval.action = api;
+    return new Promise(async (resolve, reject) => {
+      if (typeof params === "object") {
+        // Check the array.
+        for (let key in params) {
+          if (params[key] === false) {
+            reject(`Missing Object Parameters. Key: ${key}`);
+          } else {
+            this.eval[key] = params[key];
+          }
+        }
+      }
+      try {
+        let res = await fetch(`${this.eval.endpoint}?${prepare.stringify(this.eval)}`);
+
+        if (res.ok) {
+          let data = await res.json();
+
+          if (data.result === "success") {
+            resolve(data);
+          } else {
+            reject(new Error(data.result + data.message));
+          }
+        } else {
+          console.error(res.status, res.statusText);
+          reject(new Error(res.statusText));
+        }
+      } catch (error) {
+        reject(error);
+      }
+    });
+  };
 }
 
-module.exports = WHMCS
+module.exports = WHMCS;
